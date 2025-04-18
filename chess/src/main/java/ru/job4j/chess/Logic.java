ddae89048fea6d72c1a48de1617045ca1997542a
underMichael <m.podddorogin@gmail.com>
2025-04-18 10:42:00 +0300

    Revert "3. Каркас шахматной доски"
    This reverts commit 0d7b8b7f1bf6d5866003fa845fe7d66c8321bb3a.

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -2,50 +2,40 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
-
 import java.util.Arrays;
 
 public final class Logic {
-	private final Figure[] figures = new Figure[32];
+    private final Figure[] figures = new Figure[32];
+    private int index = 0;
 
-	private int index = 0;
+    public void add(Figure figure) {
+        figures[index++] = figure;
+    }
 
-	public void add(Figure figure) {
-		figures[index++] = figure;
-	}
+    public void move(Cell source, Cell dest)
+            throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
+        int index = findBy(source);
+        Cell[] steps = figures[index].way(dest);
+        free(steps);
+        figures[index] = figures[index].copy(dest);
+    }
 
-	public void move(Cell source, Cell dest) throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
-		int index = findBy(source);
+    private boolean free(Cell[] steps) throws OccupiedCellException {
+        return true;
+    }
 
-		Cell[] steps = figures[index].way(dest);
-		free(steps);
-		figures[index] = figures[index].copy(dest);
-	}
+    public void clean() {
+        Arrays.fill(figures, null);
+        index = 0;
+    }
 
-	private boolean free(Cell[] steps) throws OccupiedCellException {
-		boolean result = true;
-		for (Cell step : steps) {
-			try {
-				findBy(step);
-				throw new OccupiedCellException();
-			} catch (FigureNotFoundException e) {
-			}
-		}
-		return true;
-	}
-
-	public void clean() {
-		Arrays.fill(figures, null);
-		index = 0;
-	}
-
-	private int findBy(Cell cell) throws FigureNotFoundException {
-		for (int index = 0; index != figures.length; index++) {
-			Figure figure = figures[index];
-			if (figure != null && figure.position().equals(cell)) {
-				return index;
-			}
-		}
-		throw new FigureNotFoundException("Figure not found on the board.");
-	}
+    private int findBy(Cell cell) throws FigureNotFoundException {
+        for (int index = 0; index != figures.length; index++) {
+            Figure figure = figures[index];
+            if (figure != null && figure.position().equals(cell)) {
+                return index;
+            }
+        }
+        throw new FigureNotFoundException("Figure not found on the board.");
+    }
 }
